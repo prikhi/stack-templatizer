c321822ee58694b55d950cc9c2e15d4c6b7110ab
Pavan Rikhi <pavan.rikhi@gmail.com>
2020-02-11 11:13:08 -0500

    Add Templatizing Code
    Add the initial implementation of the templatizer. This recurses
    through the directory, collecting the filenames and contents and
    transforms it into an `hsfiles` file.

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # stack-templatizer
+
+Stack Templatizer is a small application that lets you generate stack template
+`hsfiles` from a folder.
+
+Install or clone & build the project using `stack`:
+
+```
+# Install from Stack Nightly
+stack install stack-templatizer --nightly
+
+# Or build and install from source
+git clone https://github.com/prikhi/stack-templatizer
+cd stack-templatizer
+stack install
+```
+
+Once installed, you can run `stack-templatizer my-template-folder` to generate
+a `my-template-folder.hsfiles` stack template.
+
+
+# LICENSE
+
+BSD-3-Clause
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,60 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
 module Main where
 
+import           System.Directory               ( listDirectory
+                                                , doesDirectoryExist
+                                                )
+import           System.Environment             ( getArgs )
+import           System.Exit                    ( exitFailure )
+import           System.FilePath                ( (</>) )
+
+import qualified Data.ByteString.Lazy          as LBS
+import qualified Data.ByteString.Lazy.Char8    as LC
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = getArgs >>= \case
+    [folderName] ->
+        templatize folderName >>= LBS.writeFile (folderName ++ ".hsfiles")
+    _ -> printHelp >> exitFailure
+
+
+printHelp :: IO ()
+printHelp = mapM_
+    putStrLn
+    [ "stack-templatizer: Generate Stack Templates from a Folder"
+    , ""
+    , "Usage: stack-templatizer FOLDER_NAME"
+    , ""
+    , "The generated file will be named `<folder-name>.hsfiles`"
+    ]
+
+
+templatize :: FilePath -> IO LBS.ByteString
+templatize folder = do
+    fileNames        <- getFilesInDirectory folder
+    namesAndContents <- mapM (\file -> (file, ) <$> LBS.readFile file) fileNames
+    return $ generateHFiles namesAndContents
+
+
+getFilesInDirectory :: FilePath -> IO [FilePath]
+getFilesInDirectory = recursiveList ""
+  where
+    recursiveList :: String -> FilePath -> IO [FilePath]
+    recursiveList parentDir path = do
+        let fullPath = parentDir </> path
+        isDirectory <- doesDirectoryExist fullPath
+        if isDirectory
+            then do
+                files <- listDirectory fullPath
+                concat <$> mapM (recursiveList fullPath) files
+            else return [fullPath]
+
+
+generateHFiles :: [(FilePath, LBS.ByteString)] -> LBS.ByteString
+generateHFiles = LBS.intercalate "\n\n" . map prefixName
+  where
+    prefixName :: (FilePath, LBS.ByteString) -> LBS.ByteString
+    prefixName (file, contents) =
+        "{-# START_FILE " <> LC.pack file <> " #-}\n" <> contents
